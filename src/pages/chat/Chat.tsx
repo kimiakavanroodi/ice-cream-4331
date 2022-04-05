fb57a597be29f34548da299a25d4d99dd915f6a4
Kpmia <57331532+Kpmia@users.noreply.github.com>
2022-04-05 14:11:42 -0400

    create top banner & svg icons

--- src/pages/chat/Chat.tsx
+++ src/pages/chat/Chat.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import "../../styles/chat/chat-page.css"
 
+import ClothingHanger from "../../assets/chat/clothing-hanger.svg";
+import CredCard from "../../assets/chat/credit-card.svg";
+import Calendar from "../../assets/chat/calendar-today.svg";
+import VideoChat from "../../assets/chat/video-cam.svg";
+
+
 export const ChatPage = ({...restProps} : any) => {
 
     return (
@@ -28,31 +34,72 @@ export const ChatPage = ({...restProps} : any) => {
 
                         <div className="chat-page-top-banner">
 
+                            <div className="chat-page-top-banner-left-container">
+
+                                <div style={{display: "flex", flexDirection: "column"}}>
+                                    <div className="chat-page-top-container-left-container-user-icon-bg">
+                                        <img className="chat-page-top-container-left-container-user-icon" style={{borderRadius: '100%'}} src={require('../../assets/chat/user-icon-sample.jpg')} />
+                                    </div>
+                                </div>
+
+                                <div className="chat-page-top-banner-left-container--p">
+                                    <p className="chat-page-top-banner-left-container--name"> Jason Times </p>
+                                    <p className="chat-page-top-banner-left-container--last-seen"> Last seen 16 minutes ago </p>
+                                </div>
+
+                            </div>
+
+
+                            <div className="chat-page-top-banner-right-container">
+
+                                <div className="chat-page-top-banner-right-container-icons"> 
+                                    <img src={Calendar} />
+                                </div>
+
+                                <div className="chat-page-top-banner-right-container-icons"> 
+                                    <img src={VideoChat} />
+                                </div>          
+                                
+                                <div className="chat-page-top-banner-right-container-icons"> 
+                                    <img src={CredCard} />
+                                </div>
+
+                                <div className="chat-page-top-banner-right-container-icons"> 
+                                    <img src={ClothingHanger} />
+                                </div>
+
+                            </div>
+
                         </div>
 
                         <div className="chat-page-messages-body">
 
+                        <div className="chat-page-text-sender-container">
+                                <div className="chat-page-text-sender">
+                                    <p className="chat-page-main-text-sender"> Unfortunately, I cannot change it from now. You could always schedule another session to change the outfits. </p>
+                                    <p className="chat-page-text-sender-date"> 10:42 AM </p>
+                                </div>
+                            </div>
+                            
+
                                 <div className="chat-page-text-recipient-container">
                                     <div className="chat-page-text-recipient">
-                                        <p className="chat-page-text-recipient-message "> Unfortunately, I cannot change it from now. You could always schedule another session to change the outfits. </p>
-                                        <p className="chat-page-text-recipient-date"> 10:42 AM </p>
+                                        <p className="chat-page-text-recipient-message "> is it possible to change some of the outfits now? I have new inspirational photos that can help out. </p>
+                                        <p className="chat-page-text-recipient-date"> 9:32 AM </p>
                                     </div>
                                 </div>
 
-                                <div className="chat-page-text-sender-container">
+                                <div className="chat-page-text-recipient-container">
                                     <div className="chat-page-text-recipient">
-                                        <p className="chat-page-text-recipient-message "> Unfortunately, I cannot change it from now. You could always schedule another session to change the outfits. </p>
+                                        <p className="chat-page-text-recipient-message "> hey, is there any update with the outfits? </p>
                                         <p className="chat-page-text-recipient-date"> 10:42 AM </p>
                                     </div>
                                 </div>
 
-                            <div className="chat-page-text-recipient">
-                                <p className="chat-page-text-recipient-message "> Unfortunately, I cannot change it from now. You could always schedule another session to change the outfits. </p>
-                                <p className="chat-page-text-recipient-date"> 10:42 AM </p>
-                            </div>
-
-                            <div className="">
-
+                            <div className="chat-page-large-date-container">
+                                <div className="chat-page-date-background"> 
+                                    <p className="chat-page-date--text"> March 15 </p>
+                                </div>
                             </div>
                             
                             <div className="chat-page-text-sender-container">
@@ -61,6 +108,7 @@ export const ChatPage = ({...restProps} : any) => {
                                     <p className="chat-page-text-sender-date"> 5:32 PM </p>
                                 </div>
                             </div>
+
                             <div className="chat-page-text-sender-container">
                                 <div className="chat-page-text-sender">
                                     <p className="chat-page-main-text-sender"> Great! I love hearing that. </p>
@@ -81,8 +129,9 @@ export const ChatPage = ({...restProps} : any) => {
                     
                         <div className="chat-page-message-container">
                             <div className="chat-page-message-text">
+
                                 <div>
-                                    Icon
+                                    I<img src={require("../../assets/chat/vector.svg")} />
                                 </div>
 
                                 <div className="chat-page-input-box-padding ">
