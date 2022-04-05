c88df937a7725df13ba42788e9be0e2ef8430469
Kpmia <57331532+Kpmia@users.noreply.github.com>
2022-04-05 11:10:36 -0400

    add flex boxes for inner chat container

--- src/pages/chat/Chat.tsx
+++ src/pages/chat/Chat.tsx
@@ -32,16 +32,47 @@ export const ChatPage = ({...restProps} : any) => {
 
                         <div className="chat-page-messages-body">
 
-                        <div className="chat-page-text">
-                                <p> Unfortunately, I cannot change it from now. You could always schedule another session to change the outfits. </p>
-                            </div>
-                            
-                        <div className="chat-page-text">
-                                <p> Unfortunately, I cannot change it from now. You could always schedule another session to change the outfits. </p>
+                                <div className="chat-page-text-recipient-container">
+                                    <div className="chat-page-text-recipient">
+                                        <p className="chat-page-text-recipient-message "> Unfortunately, I cannot change it from now. You could always schedule another session to change the outfits. </p>
+                                        <p className="chat-page-text-recipient-date"> 10:42 AM </p>
+                                    </div>
+                                </div>
+
+                                <div className="chat-page-text-sender-container">
+                                    <div className="chat-page-text-recipient">
+                                        <p className="chat-page-text-recipient-message "> Unfortunately, I cannot change it from now. You could always schedule another session to change the outfits. </p>
+                                        <p className="chat-page-text-recipient-date"> 10:42 AM </p>
+                                    </div>
+                                </div>
+
+                            <div className="chat-page-text-recipient">
+                                <p className="chat-page-text-recipient-message "> Unfortunately, I cannot change it from now. You could always schedule another session to change the outfits. </p>
+                                <p className="chat-page-text-recipient-date"> 10:42 AM </p>
                             </div>
 
-                            <div className="chat-page-text">
-                                <p> Unfortunately, I cannot change it from now. You could always schedule another session to change the outfits. </p>
+                            <div className="">
+
+                            </div>
+                            
+                            <div className="chat-page-text-sender-container">
+                                <div className="chat-page-text-sender">
+                                    <p className="chat-page-main-text-sender"> Hey, just an update: I have created the last outfit. I’ll upload it later in the chat tomorrow night. </p>
+                                    <p className="chat-page-text-sender-date"> 5:32 PM </p>
+                                </div>
+                            </div>
+                            <div className="chat-page-text-sender-container">
+                                <div className="chat-page-text-sender">
+                                    <p className="chat-page-main-text-sender"> Great! I love hearing that. </p>
+                                    <p className="chat-page-text-sender-date"> 5:30 PM </p>
+                                </div>
+                            </div>
+
+                            <div className="chat-page-text-recipient-container">
+                                <div className="chat-page-text-recipient">
+                                    <p className="chat-page-text-recipient-message ">Love the second outfit. it looks amazing - cannot wait to get it. </p>
+                                    <p className="chat-page-text-recipient-date"> 3:42 PM </p>
+                                </div>
                             </div>
 
 
