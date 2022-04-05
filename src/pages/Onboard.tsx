2b7813ca7077ded40e4b4c5ed2ff8c16b3faade4
Florence Omu <fomu3@gatech.edu>
2022-04-05 18:52:04 -0400

    creating card
    creating cards and button

--- src/pages/Onboard.tsx
+++ src/pages/Onboard.tsx
@@ -1,23 +1,33 @@
+import { logRoles } from '@testing-library/react';
 import React from 'react';
+import '../styles/Onboard.css';
+import WhiteLogo from '../assets/onboarding/whitelogo.svg';
+
 
 function OnBoard() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Onboarding process
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
+  return ( 
+  <div style={{height: "100vh", background: "linear-gradient(140.42deg, #101214 11.23%, #131516 26.36%, #16161A 47.52%, #101214 66.88%, #1D1D23 84.53%)"}}>
+      <div className="OnboardContainer"> 
+        <div className="OnboardCard">
+          <div className="WhiteLogoContainer">
+            <img src={WhiteLogo}/>            
+          </div>
+          <div className="ButtonsContainer">
+             <button className="BackBtn">
+               Back
+               </button> 
+               <button className="NextStepBtn">
+                Next Step
+               </button>
+          </div>
+        </div>
+      </div>
+  </div>
+
+);
+  
+  
 }
 
 export default OnBoard;
