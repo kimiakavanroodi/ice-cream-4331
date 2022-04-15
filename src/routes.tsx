f63f93f7be5ff66cb50ced72c30a374eceec1106
Florence Omu <fomu3@gatech.edu>
2022-04-14 21:05:08 -0400

    Onboard (#2)
    * creating card
    
    creating cards and button
    
    * adding onboarding
    
    adding onboarding

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,7 +2,7 @@ import './styles/App.css';
 import React, { useState } from "react"
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import MainBody from './pages/App';
-import OnBoard from './pages/Onboard';
+import { OnBoard } from './pages/Onboard';
 import { Login } from './pages/Login';
 
 const App = ({...restProps}) => {
